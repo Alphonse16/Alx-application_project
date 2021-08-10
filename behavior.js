38e14bb95b7f751b32fca085442494dae62a2d5e
Mondliwethu Alphonse Hadebe <hmondliwethu@gmail.com>
2021-08-10 15:46:14 +0200

    HTML, CSS & JS project

---
+++ behavior.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+
+    thumbnailElement.addEventListener("click", function() {
+    if (this.className === "small")
+        this.className = "";
+    else
+        this.className = "small";
+        });
+
+
+    var thumbnailElement = document.getElementById("smart_thumbnail1");
+
+    thumbnailElement.addEventListener("click", function() {
+        if (this.className === "small")
+            this.className = "";
+        else
+            this.className = "small";
+            });
+
+    
+    var thumbnailElement = document.getElementById("smart_thumbnail2");
+
+    thumbnailElement.addEventListener("click", function() {
+        if (this.className === "small")
+            this.className = "";
+        else
+            this.className = "small";
+            });        
+
+    
+    var thumbnailElement = document.getElementById("smart_thumbnail3");
+
+    thumbnailElement.addEventListener("click", function() {
+        if (this.className === "small")
+            this.className = "";
+        else
+            this.className = "small";
+            });
+
+    var thumbnailElement = document.getElementById("smart_thumbnail4");
+
+    thumbnailElement.addEventListener("click", function() {
+        if (this.className === "small")
+            this.className = "";
+        else
+            this.className = "small";
+            });
+        
+
+});
+
